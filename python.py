612ed2ae7f0c6d0006c62a53ee378c5cabaf3c78
nostalgic <kyleelzy07@gmail.com>
2022-08-25 13:20:18 -0500

    made transparent function work

--- python.py
+++ python.py
@@ -3,22 +3,45 @@ from operator import length_hint
 from pathlib import Path
 import cv2
 import numpy as np
+from PIL import Image, ImageEnhance, ImageChops
 
+#loads config file
 fi = "config.ini"
 config = configparser.RawConfigParser()
 config.read('config.ini')
 with open(fi, 'w') as configfile:
     config.write(configfile)
 
-from PIL import Image, ImageEnhance
+#splits image into the three color channels
 img = cv2.imread('yourimage.png', cv2.IMREAD_UNCHANGED)
-green_channel = img[:,:,1]
-cv2.imwrite('green_channel.png',green_channel)
-red_channel = img[:,:,2]
-cv2.imwrite('red_channel.png',red_channel)
-blue_channel = img[:,:,0]
-cv2.imwrite('blue_channel.png',blue_channel)
+cv2.imwrite('green_channel.png',img[:,:,1])
+cv2.imwrite('red_channel.png',img[:,:,2])
+cv2.imwrite('blue_channel.png',img[:,:,0])
 
+#increases contrast on each the color channel images
+ImageEnhance.Contrast(Image.open("red_channel.png")).enhance(15).save('red_channel.png')
+ImageEnhance.Contrast(Image.open("green_channel.png")).enhance(15).save('green_channel.png')
+ImageEnhance.Contrast(Image.open("blue_channel.png")).enhance(15).save('blue_channel.png')
+def convertImage(imageprob):
+    img = Image.open(imageprob).convert("RGBA")
+ 
+    datas = img.getdata()
+ 
+    newData = []
+ 
+    for item in datas:
+        if item[0] == 0 and item[1] == 0 and item[2] == 0:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+ 
+    img.putdata(newData)
+    img.save(imageprob, "PNG")
+    print("Successful")
+convertImage("red_channel.png")
+convertImage("green_channel.png")
+convertImage("blue_channel.png")
+#resizes image
 x, y, c = cv2.imread('yourimage.png').shape
 print(x)
 print(y)
